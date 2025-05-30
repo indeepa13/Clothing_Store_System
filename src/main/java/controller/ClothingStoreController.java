292af5c4b6dec3827604e4a9d6de097fe5648592
indeepa13 <indeepayasangith13@gmail.com>
2025-05-30 19:44:56 +0530

    DBand CRUD

--- src/main/java/controller/ClothingStoreController.java
+++ src/main/java/controller/ClothingStoreController.java
@@ -1,5 +1,51 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+
 public class ClothingStoreController {
+
+    @FXML
+    private ChoiceBox choiCategory;
+
+    @FXML
+    private TableColumn colCategory;
+
+    @FXML
+    private TableColumn colEmployee;
+
+    @FXML
+    private TableColumn colProduct;
+
+    @FXML
+    private JFXTextField txtEmployee;
+
+    @FXML
+    private JFXTextField txtProduct;
+
+    @FXML
+    void AddClickOnActon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteClickOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SearchOnClickAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UpdateClickOnAction(ActionEvent event) {
+
+    }
+
+
 
 }

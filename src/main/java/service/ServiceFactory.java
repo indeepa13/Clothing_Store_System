7616d373a3f9d895d7a52c6b263ab23da0d98bfc
indeepa13 <indeepayasangith13@gmail.com>
2025-08-08 14:50:48 +0530

    db create login

---
+++ src/main/java/service/ServiceFactory.java
@@ -0,0 +1,26 @@
+package service;
+
+import service.custom.EmployeeService;
+import service.custom.impl.EmployeeServiceImpl;
+import service.custom.impl.ProductServiceImpl;
+import service.custom.impl.UserServiceImpl;
+import util.ServiceType;
+
+
+public class ServiceFactory {
+    private static ServiceFactory instance;
+    private ServiceFactory(){}
+
+    public static ServiceFactory getInstance(){
+        return instance==null?instance = new ServiceFactory():instance;
+    }
+
+    public <T extends SuperService> T getServiceType(ServiceType type){
+        switch (type){
+            case EMPLOYEE:return (T) new EmployeeServiceImpl();
+            case PRODUCT:return  (T) new ProductServiceImpl();
+            case USER:return  (T) new UserServiceImpl();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/service/custom/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package service.custom.impl;
+
+import model.dto.EmployeeDTO;
+import model.entity.EmployeeEntity;
+import org.modelmapper.ModelMapper;
+import repository.DaoFactory;
+import repository.custom.EmployeeDAO;
+import service.custom.UserSerrvice;
+import util.RepositoryType;
+
+import java.sql.SQLException;
+
+public class UserServiceImpl implements UserSerrvice {
+    EmployeeDAO employeeDAO = DaoFactory.getInstance().getRepositoryType(RepositoryType.EMPLOYEE);
+
+    @Override
+    public EmployeeDTO loginUser(String username, String password) throws SQLException {
+//        EmployeeEntity employeeEntity = employeeDAO.searchById(username);
+//        EmployeeDTO employeeDTO = new ModelMapper().map(employeeEntity, EmployeeDTO.class);
+//        if (employeeDTO.getPassword().equals(password)){
+//            return employeeDTO;
+//        }
+        return null;
+    }
+}
